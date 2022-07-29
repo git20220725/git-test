459ea173d05017b8109f78104bfd01c03d9a9ffd
lisheng <3274930691@qq.com>
2022-07-29 18:26:37 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -2,10 +2,10 @@ package com.atguigu.git;
 
 /**
  * @author lsstart
- * @create 2022-07-29 18:19
  */
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git~");
+        System.out.println("hello git2~");
     }
 }
