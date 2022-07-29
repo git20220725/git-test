c225bdba338d10a723865509f4fc0b70b205673f
lisheng <3274930691@qq.com>
2022-07-29 18:23:31 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author lsstart
+ * @create 2022-07-29 18:19
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git~");
+    }
+}
